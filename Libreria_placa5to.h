f0d948fa1947460e067894bd54ea44538cebc6b2
marco molina <mythicalboye@gmail.com>
2022-06-19 03:25:25 -0300

    Versión 1.0

--- Libreria_placa5to.h
+++ Libreria_placa5to.h
@@ -1,28 +1,44 @@
-
+//λ Variables Globales λ//
 int PDState;
 int PUState;
+
+
+//λ Entradas/Inputs λ//
 #define PD 2
 #define PU 3
+
+#define pot1 A0 //Valor de 1K
+#define pot2 A1 //Valor de 1k
+
+//λ Salidas/Outputs λ//
 #define LED1 13
 #define LED2 12
+
+//λ Estados λ//
 #define ON true
 #define OFF false
 
-//********************PROTOTIPO************************
+//λ********************PROTOTIPO DE FUNCIONES************************λ
 
-void DeclararPines ();
+void DeclararPines (void);
  
-int LeerPulsadores ();
+int LeerPulsadores (void);
 
-void EstadoLed(int, bool);
+float LeerPotenciometros(void); //Lee los 2 potenciómetros y devuelve la suma de esos 2 valores
 
-//********************CUERPO************************
+void EstadoLed(int, bool); //Lee el número de led asignado y envía el valor ingresado al mismo
 
-void DeclararPines(){
+//********************CUERPO DE FUNCIONES************************
+
+void DeclararPines(void){
   pinMode(PU, INPUT);
   pinMode(PD, INPUT);
   pinMode(LED1, OUTPUT);
   pinMode(LED2, OUTPUT);
+  for (int i = 4; i < 11 ; i++) //Declaraciones de pines digitales OPCIONALES macho de salida incluidos en la placa
+  {
+  pinMode(i,OUTPUT);
+  }
 }
 
 void EstadoLed(int num, bool state){
@@ -38,7 +54,7 @@ void EstadoLed(int num, bool state){
 digitalWrite(num, state);
 }
 
-int LeerPulsadores(){
+int LeerPulsadores(void){
 
 PDState = digitalRead(PD);
 PUState = digitalRead(PU);
@@ -55,3 +71,11 @@ PUState = digitalRead(PU);
   if (PDState == 0 || PUState == 1){
   return (3);
   }}}}}
+
+  float LeerPotenciometros(){
+    float val, P1, P2;
+    P1 = analogRead(pot1);
+    P2 = analogRead(pot2);
+    val = (P1 + P2);
+    return (val);
+  }
